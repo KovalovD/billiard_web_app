5461f9d0a17edb8e7fddfa63716f45566da09e5a
Dmytro Kovalov <dimon199724@gmail.com>
2025-05-04 18:42:56 +0300

    Add foreign key constraints and prevent duplicate matches
    Foreign key constraints were added to relevant migration files to ensure referential integrity in the database. Additionally, a check was implemented in the `MatchGamesService` to prevent creating consecutive matches between the same opponents. These changes enhance data consistency and improve match validation logic.

--- database/migrations/2001_04_21_173303_create_cities_table.php
+++ database/migrations/2001_04_21_173303_create_cities_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
         Schema::create('cities', static function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(Country::class);
+            $table->foreignIdFor(Country::class)->constrained();
         });
     }
 
--- database/migrations/2024_04_22_115358_create_leagues_table.php
+++ database/migrations/2024_04_22_115358_create_leagues_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
         Schema::create('leagues', static function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(Game::class)->nullable();
+            $table->foreignIdFor(Game::class)->nullable()->constrained();
             $table->string('picture')->nullable();
             $table->text('details')->nullable();
             $table->string('rating_type')->default('elo');
