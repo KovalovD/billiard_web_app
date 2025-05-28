1d260ae6728b271d66278b879248d2cead63a196
Dmitriy Kovalov <d.kovalov@sda.company>
2025-05-28 22:28:19 +0300

    Add initial migration and seed data setup
    Introduces an initial database migration that triggers data seeding and tournament data import from `public/import.xlsx`. Adjusts the migration filename for updating the users table to maintain chronological consistency.

--- database/migrations/2025_05_28_185918_init_migration.php
+++ database/migrations/2025_05_28_185918_init_migration.php
@@ -5,7 +5,9 @@ use Illuminate\Database\Migrations\Migration;
 return new class extends Migration {
     public function up(): void
     {
-        Artisan::call('db:seed');
-        Artisan::call('import:tournaments public/import.xlsx');
+        if (app()->environment('production')) {
+            Artisan::call('db:seed');
+            Artisan::call('import:tournaments public/import.xlsx');
+        }
     }
 };
