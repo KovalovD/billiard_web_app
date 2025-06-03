c05830243bd0fbac801edc18016ca8153a824134
Dmitriy Kovalov <d.kovalov@sda.company>
2025-06-03 20:00:41 +0300

    Replace logo image with text and rename favicon
    Replaced the application logo image with a text-based alternative for simplicity and flexibility. Additionally, renamed the favicon file for better clarity and consistency in naming conventions.

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,19 +1,12 @@
 <script lang="ts" setup>
-// ИМПОРТ НЕ НУЖЕН, так как файл в public
 
-// defineOptions не обязателен, но полезен для Vue DevTools
 defineOptions({
     name: 'ApplicationLogo',
 });
 
-// Можно оставить пропсы для размера, если они нужны
-// interface Props {
-//   width?: string | number;
-//   height?: string | number;
-// }
-// const props = defineProps<Props>();
 </script>
 
 <template>
-    <img alt="Application Logo" src="/logo-min.PNG" v-bind="$attrs"/>
+    <span class="content-center">WinnerBreak</span>
+    <!--    <img alt="Application Logo" src="/logo-min.PNG" v-bind="$attrs"/>-->
 </template>
