40a3fc45822ba3943e18515a1772fcd3efefccfb
Dmytro Kovalov <dimon199724@gmail.com>
2025-07-14 23:15:34 +0300

    Add random inspiring quote to shared Inertia data in middleware
    - Added `Inspiring` class usage to fetch random quotes.
    - Updated `share` method to include a formatted quote object with message and author in Inertia shared data.

--- app/Core/Http/Middleware/HandleInertiaRequests.php
+++ app/Core/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Core\Http\Middleware;
 
 use App\User\Http\Resources\UserResource;
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
@@ -37,9 +38,12 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
+
         return [
             ...parent::share($request),
             'name'        => config('app.name'),
+            'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth'        => [
                 'user' => new UserResource($request->user()),
             ],
